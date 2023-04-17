c6fa2ca2e25358ea94400696d85d75b04df785d8
roberte777 <robewilkes@yahoo.com>
2023-04-16 20:32:17 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+ok here basic structure
+
+I need to create a table that has
+
+- user
+- balance
---
+++ src/main.rs
@@ -0,0 +1,53 @@
+use std::env;
+use dotenv::dotenv;
+use serenity::model::gateway::Ready;
+
+
+use serenity::async_trait;
+use serenity::prelude::*;
+use serenity::model::channel::Message;
+use serenity::framework::standard::macros::{command, group};
+use serenity::framework::standard::{StandardFramework, CommandResult};
+
+#[group]
+#[commands(bean)]
+struct General;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+   async fn ready(&self, _: Context, ready: Ready) {
+       println!("{} is connected!", ready.user.name);
+   }
+}
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+    // https://docs.rs/serenity/0.11.5/serenity/framework/standard/struct.StandardFramework.html
+    let framework = StandardFramework::new()
+        .configure(|c| c.prefix("/").with_whitespace(true)) // set the bot's prefix to "~"
+        .group(&GENERAL_GROUP);
+
+    // Login with a bot token from the environment
+    let token = env::var("DISCORD_TOKEN").expect("token");
+    let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
+    let mut client = Client::builder(token, intents)
+        .event_handler(Handler)
+        .framework(framework)
+        .await
+        .expect("Error creating client");
+
+    // start listening for events by starting a single shard
+    if let Err(why) = client.start().await {
+        println!("An error occurred while running the client: {:?}", why);
+    }
+}
+
+#[command]
+async fn bean(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.reply(ctx, "FAT!").await?;
+
+    Ok(())
+}
